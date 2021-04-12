6627bd2d4894ff6431f47fa099da1b7a9b541e68
Eric Lars Lee <ericlarslee@gmail.com>
2021-04-12 11:37:17 -0500

    Created for form for employees that is automatically directed to upon first login as employee to enter information

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('update_account/', views.update, name="update")
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse, get_object_or_404
+from .models import Employee
 from django.apps import apps
 from datetime import date
 import calendar
-from trash_collector.customers.models import Customer
+from django.forms import ModelForm
+from django.contrib.auth.models import Group
 
 
 # Create your views here.
@@ -13,8 +15,59 @@ from trash_collector.customers.models import Customer
  #this index seems redundant and unneeded compared to the simple import
 def index(request):
     # Get the Customer model from the other app, it can now be used to query the db
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    user = request.user
+    employees = Employee.objects.all()
+    if len(employees) == 0:
+        return render(request, 'employees/update_account.html')
+    else:
+        for employee in employees:
+            if employee.user_id == user.pk:
+                employee = Employee.objects.get(user=user.id)
+                if employee.zipcode is None:
+                    return render(request, 'employees/update_account.html')
+                else:
+                    Customer = apps.get_model('customers.Customer')
+                    customers = Customer.objects.all()
+                    customer_list = []
+                    today = day_name()
+                    for customer in customers:
+                        if customer.pickup_day == today:
+                            customer_list.append(customer)
+                    for customer in customer_list:
+                        if customer.zipcode != employee.zipcode:
+                            customer_list.remove(customer)
+                        if not customer.account_status:
+                            customer_list.remove(customer)
+                    context = {
+                        'customers': customer_list,
+                        'employee': employee
+                    }
+                    return render(request, 'employees/index.html', context)
+
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['name', 'zipcode']
+        labels = {
+            'name': 'Name',
+            'zipcode': 'Zipcode'
+        }
+
+
+def update(request):
+    user = request.user
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        zipcode = request.POST.get('zipcode')
+        new_employee = Employee(name=name, user=user, zipcode=zipcode)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'employee': employee
+        }
+        return render(request, 'employees/update_account.html', context)
 
 
 def day_name():
@@ -24,7 +77,8 @@ def day_name():
 
 
 def today_customer_list(request):
-    customers = Customer.objects.all()
+    customer_group = Group.objects.get(name="Customers")
+    customers = customer_group.user_set_all()
     employee = request.user
     customer_list = []
     today = day_name()
@@ -43,7 +97,8 @@ def today_customer_list(request):
 
 
 def daily_customer_list(request):
-    customers = Customer.objects.all()
+    customer_group = Group.objects.get(name="Customers")
+    customers = customer_group.user_set_all()
     employee = request.user
     customer_today_list = []
     today = day_name()
